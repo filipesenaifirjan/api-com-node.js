a381936c7b8c43a582ee48f3af48e62ec2893ab0
filipesenaifirjan <filipemattossenai@gmail.com>
2023-06-22 10:28:45 -0300

    routes

--- backend/controllers/serviceController.js
+++ backend/controllers/serviceController.js
@@ -6,8 +6,14 @@ const serviceController = {
     create: async(req, res) => {
         try {
             const service = {
-                
+                name: req.body.name,
+                description: req.body.description,
+                price: req.body.price,
+                image: req.body.image,
             };
+
+            const response = await ServiceModel.create(service);
+            res.status(201).json({ response, msg: "Serviço criado com sucesso!" })
             } catch (error) {
                 console.log(error);
             }
